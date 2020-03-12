09a8ca8343efbc0ec83afd567de3052524be1833
MBANS8A1 <62100017+MBANS8A1@users.noreply.github.com>
2020-03-12 11:22:35 +0000

    Update MRequest.java

--- MRequest.java
+++ MRequest.java
@@ -9,3 +9,16 @@ Based off what I read option number 3 is common and recommended while option 2 i
 
 Language: decided on Java since test is for Java position
 */
+
+//Next import Libraries for Java
+import java.util.*; 
+import java.lang.Math; 
+import java.io.*;
+import java.net.URI;
+import java.net.ProxySelector;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.*;
+//Libraries imported
+
