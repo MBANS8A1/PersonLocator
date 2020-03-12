d8a05b5d471db6c5229dbe6ef654b05204eedb4b
MBANS8A1 <62100017+MBANS8A1@users.noreply.github.com>
2020-03-12 11:20:50 +0000

    Update MRequest.java

--- MRequest.java
+++ MRequest.java
@@ -1,1 +1,11 @@
+//Firstly decide on how to make HTTP(S)requests
+/*
+Can use:
+1.HTTPURLConnection
+2.Apache HTTP Client 
+3. Java 11 HTTP/2 HTTP Client supports earlier HTTP/1.1 Too
 
+Based off what I read option number 3 is common and recommended while option 2 is reasonable but older 
+
+Language: decided on Java since test is for Java position
+*/
